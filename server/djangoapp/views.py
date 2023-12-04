31fa55523c721268a29211e70319fc96bcb61ca8
Airdm404 <edemcapp101@gmail.com>
2023-12-04 02:29:01 -0500

    create django view to get reviews of a dealer

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 from datetime import datetime
 import logging
 import json
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, get_dealer_reviews_from_cf
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -112,6 +112,7 @@ def registration_request(request):
 #         # Return a list of dealer short name
 #         return HttpResponse(dealer_names)
 
+
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
@@ -121,8 +122,20 @@ def get_dealerships(request):
         return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://edemahorlu-3000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        dealer = get_dealer_by_id_from_cf(dealer_url, id=dealer_id)
+        
+        context["dealer"] = dealer
+    
+        review_url = "https://edemahorlu-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+        reviews = get_dealer_reviews_from_cf(review_url, id=dealer_id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
